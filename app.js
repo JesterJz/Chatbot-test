005dbe3df1fce607ed56ccedf25fe405c7243598
Jester Jz <45939805+JesterJz@users.noreply.github.com>
2019-11-12 15:11:58 +0700

    Update app.js

--- app.js
+++ app.js
@@ -30,7 +30,7 @@ const
   app = express().use(body_parser.json()); // creates express http server
 
 // Sets server port and logs message on success
-app.listen(process.env.PORT || 1337, () => console.log('webhook is listening'));
+app.listen(process.env.PORT || 1338, () => console.log('webhook is listening'));
 
 // Accepts POST requests at /webhook endpoint
 app.post('/webhook', (req, res) => {  
